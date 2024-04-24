869a73f6954635a89b490b31efb9c58fbbef8c59
Przemyslaw <surichbuncher@gmail.com>
2024-04-24 16:11:46 +0200

    VA06 rtg

---
+++ MediaPlayer_VA06/src/tescik.java
@@ -0,0 +1,13 @@
+
+public class tescik {
+	private int x = 2;
+void parseName() {
+	
+	System.out.println("chuj");
+}
+	
+	public static void main(String[] args) {
+	tescik test = new tescik();
+	System.out.println(test.x);
+	}
+}
